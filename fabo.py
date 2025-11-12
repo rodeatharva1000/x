03f1535910b38e7942275190c2abc8271defd779
rodeatharva1000 <123895899+rodeatharva1000@users.noreply.github.com>
2025-11-12 18:35:33 +0530

    Create fabo.py

---
+++ fabo.py
@@ -0,0 +1,23 @@
+store = {}
+def fabo(n):
+    if n <= 1:
+        store[n] = n
+        return n
+    if n in store:
+        return store[n]
+    res = fabo(n-1)+fabo(n-2)
+    store[n] = res
+    return res
+fabo(500)
+
+for x, y in sorted(store.items()):
+    print(x, y)
+
+n = 100
+a = -1
+b = 1
+for i in range(n):
+    c = a+b
+    print(c)
+    a = b
+    b = c
