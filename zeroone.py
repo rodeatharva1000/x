1f19190a43e23b5d713a1fab74542128143307f9
rodeatharva1000 <123895899+rodeatharva1000@users.noreply.github.com>
2025-11-15 11:28:59 +0530

    Implement memoized knapsack function
    Refactored knapsack algorithm using memoization.

--- zeroone.py
+++ zeroone.py
@@ -17,3 +17,34 @@ for i in range(1, n + 1):
             dp[i][cap] = dp[i-1][cap]              # can't take item
 
 print(dp[n][c])
+
+
+
+
+
+
+from functools import cache
+
+w = [0, 40, 30, 90, 20]
+v = [0, 7, 20, 10, 1]
+c = 90
+
+n = len(v) - 1
+
+@cache
+def knapsack(i, cap):
+    # no items or no capacity
+    if i == 0 or cap == 0:
+        return 0
+    
+    # if too heavy → can't include
+    if w[i] > cap:
+        return knapsack(i - 1, cap)
+    
+    # otherwise choose best
+    take = v[i] + knapsack(i - 1, cap - w[i])
+    not_take = knapsack(i - 1, cap)
+    
+    return max(take, not_take)
+
+print(knapsack(n, c))
